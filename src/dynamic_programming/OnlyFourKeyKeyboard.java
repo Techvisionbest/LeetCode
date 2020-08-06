080c093eb9f9afddb92a9ec3c0becad0ca8858fd
vision <591807656@qq.com>
2020-08-06 16:05:34 +0800

    feat: 4键键盘问题
    4键键盘题目、示例

---
+++ src/dynamic_programming/OnlyFourKeyKeyboard.java
@@ -0,0 +1,17 @@
+package dynamic_programming;
+
+/**
+ * 题目：
+ *
+ * 假设你有一个特殊的键盘包含下面的按键：
+ * Key 1：在屏幕上打印一个'A'。
+ * Key 2：(Ctrl - A)：选中整个屏幕。
+ * Key 3：(Ctrl - C)：复制选中区域到缓冲区。
+ * Key 4：(Ctrl - V)：将缓冲区内容输出到上次输入的结束位置，并显示在屏幕上。
+ * 现在，你只可以按键N次（使用上述四种按键），请问屏幕上最多可以显示几个'A'呢？
+ *
+ * 示例：
+ *
+ */
+public class OnlyFourKeyKeyboard {
+}
