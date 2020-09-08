516a70571adbc57e47a98b2bf9d9ae0fcbbe2fae
vision <591807656@qq.com>
2020-09-08 08:33:41 +0800

    feat:环形链表II

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # vision 的算法日记
-## 「动态规划」
 
-### [「腾讯」编辑距离](https://github.com/Techvisionbest/LeetCode/blob/master/src/dynamic_programming/「腾讯」编辑距离.md)
-### [「动态规划」打家劫舍：偷还是不偷，这是个问题](https://github.com/Techvisionbest/LeetCode/blob/master/src/dynamic_programming/「动态规划」打家劫舍：偷还是不偷，这是个问题.md)
-### [「爬楼梯」你(斐波那契数列)以为你穿上马甲我就不认识你了么](https://github.com/Techvisionbest/LeetCode/blob/master/src/dynamic_programming/「爬楼梯」你(斐波那契数列)以为你穿上马甲我就不认识你了么.md)
+> 自古深情留不住，总是套路得人心
+
+
+![](https://cdn.nlark.com/yuque/0/2020/png/592150/1599174239792-24b6fa9b-8289-482c-a935-1fe5a163a0c5.png)  
+
+
+## 「动态规划」系列
+
+### [「腾讯」编辑距离](https://www.yuque.com/mdeveloper/lb5slg/kqzyfb)
+### [「动态规划」打家劫舍：偷还是不偷，这是个问题](https://www.yuque.com/mdeveloper/lb5slg/gn398o)
+### [「爬楼梯」你(斐波那契数列)以为你穿上马甲我就不认识你了么](https://www.yuque.com/mdeveloper/lb5slg/bw9hqx)
 
 
 
---
+++ src/two_pointer/DetectCycle.java
@@ -0,0 +1,46 @@
+/**
+ * @link leetCode:https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ *
+ * @author vision
+ * @since 2020/09/08
+ */
+public class DetectCycle {
+
+    /**
+     * Definition for singly-linked list.
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode solution(ListNode head) {
+        boolean hasCycle = false;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast){
+                hasCycle = true;
+                break;
+            }
+        }
+
+        slow = head;
+        if (hasCycle){
+            while(slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+
+        }else {
+            return null;
+        }
+    }
+}
